d04701c453b554134ebf3b72a6990efa73e98aab
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2020-02-20 18:56:38 +0000

    pseudocode written for JS

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+//on page open, timer counts from 90 secs down
+
+//on page open, or on restart, cards are reshuffled
+
+//on reset, cards shuffled, reset game board, reset timer 
+
+//if timer = 0, game over
+
+// on click, moves increases by 1
+
+// if moves > 40, game over
+
+//on restart, pasue game and timer and ask for confirmation
+    //if confirmation = true page refreshes and timer + cards all reset
+    //if confirmation = false, game and timer resumes
+
+
+// on click:
+    //if no other card active, flip card and set to active
+    //if other card active, compare img of both active cards
+        //if img match, match counter increase by one
+        //on no match, both cards return to rest state
+
+//(no. of cards = 18)
+
+//if match counter = 9, player wins.
+
+//if match counter = 9, timer stops
+
+//score? = at win, total moves and total time remaining
+
+//set keyboard (p)ause and (esc)ape actions
+
+//if player wins, produce success message (and rating out of three?)
+
+//if player loses (out of time, or exceeds moves), produce loss message
