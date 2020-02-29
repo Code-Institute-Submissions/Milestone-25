40cbef940f5134e96e6c61d833fa1638ab6a916d
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2020-02-29 19:32:07 +0000

    click for card open and timer function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,40 @@
-//on page open, timer counts from 90 secs down
+$(document).ready(function() {
+
+// =================================CLICK TO VIEW=========================== //
+
+//what's with the wierd transfering click?
+    //on click, display card 
+    $('.card').click(function() {
+        $(this).addClass('open');
+    })
+    $('.matchedcard').click(function() {
+        $(this).addClass('open');
+    })
+
+// =================================TIMER COUNTDOWN=========================== //
+
+
+//need to sort for time rather than no.?
+  var updatetime = function() {
+    $('.timer').each(function() {
+      var count = parseInt($(this).html());
+      if (count !== 0) {
+        $(this).html(count - 1);
+      }
+    });
+  };
+  setInterval(updatetime, 1000);
+
+})
+
+
+
+
+
+
+
+
+//on page open, timer counts from 59 secs down
 
 //on page open, or on restart, cards are reshuffled
 
@@ -6,7 +42,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -26,15 +63,15 @@ function shuffle(array) {
 // if moves > 40, game over
 
 //on restart, pasue game and timer and ask for confirmation
-    //if confirmation = true page refreshes and timer + cards all reset
-    //if confirmation = false, game and timer resumes
+//if confirmation = true page refreshes and timer + cards all reset
+//if confirmation = false, game and timer resumes
 
 
 // on click:
-    //if no other card active, flip card and set to active
-    //if other card active, compare img of both active cards
-        //if img match, match counter increase by one
-        //on no match, both cards return to rest state
+//if no other card active, flip card and set to active
+//if other card active, compare img of both active cards
+//if img match, match counter increase by one
+//on no match, both cards return to rest state
 
 //(no. of cards = 18)
 
@@ -51,4 +88,3 @@ function shuffle(array) {
 //if player loses (out of time, or exceeds moves), produce loss message
 
 //
-
