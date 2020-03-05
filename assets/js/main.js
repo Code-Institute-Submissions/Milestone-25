b766f8efe8bd3e70371bd9a1748a417a6b1968e7
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2020-03-05 19:27:20 +0000

    stopped cards from auto-flipping without another card being clicked)

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,29 +39,25 @@ let addCardListener = function() {
         delay = 500;
         //if a card has already been clicked (i.e. the opened array contains a card i.e. has an array
         //length greater than one (opened.length > 1)
-        if (opened.length > 1 && card === opened[0]) {
-        //then I need to parse the gamedeck to find any item with the class 'open', and add class of 'matched'
-        //to that item.
-            $gamedeck.find('.open').addClass('matched');
-        }
-        //but only do this if that card *is already in gamedeck*.
-        //if open card is not already in game deck, I should add a 'nonmatched' class. 
-        else {
-        //okay, so once matched/nonmatched are added, the open/show class needs to be removed,
-        // so that the card closes? So, search gamedeck for open classes and remove class?
-        // $gamedeck.find('.open').removeClass('open show')
-            $gamedeck.find('.open').addClass('nonmatched');
-        //need to put some delays in to give the user some time to visually register the 
-        //events happening so delay = 500 ms
-        //maybe use settimeout?
-        //setTimeout (function () {
-        //insert event?;    
-        //}, delay);
-            setTimeout(function () {
-                $gamedeck.find('.open').removeClass('open show');    
-            }, delay);
+        if (opened.length > 1)
+            if (card === opened[0]) {
+            //then I need to parse the gamedeck to find any item with the class 'open', and add class of 'matched'
+            //to that item.
+                $gamedeck.find('.open').addClass('matched');
+            }
+            //but only do this if that card *is already in gamedeck*.
+            //if open card is not already in game deck, I should add a 'nonmatched' class. 
+            else {
+            //okay, so once matched/nonmatched are added, the open/show class needs to be removed,
+            // so that the card closes
+                $gamedeck.find('.open').addClass('nonmatched');
+            //need to put some delays in to give the user some time to visually register the events
+                setTimeout(function () {
+                    $gamedeck.find('.open').removeClass('open show');    
+                }, delay);
         };
-
+        
+        //so need cards to remain open until second card is clicked.
 
     });
 }
